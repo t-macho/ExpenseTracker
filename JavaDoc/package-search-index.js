160f27224ca27720a5c2e7c4248281a5e2f7c468
tmacho <tommymacho@seznam.cz>
2022-09-07 14:45:24 +0200

    Add javadoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"expensetracker.accounts"},{"l":"expensetracker.dialogs"},{"l":"expensetracker.filters"},{"l":"expensetracker.forms"},{"l":"expensetracker.main"},{"l":"expensetracker.table"},{"l":"expensetracker.transactions"}];updateSearchResults();
